a38726454274e25f23ebf2c382dd023e223036d1
Bozhidar83 <b.chakurov@abv.bg>
2016-02-24 23:07:49 +0200

    advanced JS homeworks

---
+++ Advanced-JavaScript/04.Simulating-OOP/Problem-4-Useful-String-Methods/script.js
@@ -0,0 +1,133 @@
+function startsWith(substring){
+    return this.indexOf(substring) === 0;
+}
+
+String.prototype.startsWith = startsWith;
+
+function endsWith(substring){
+    var substringLength = substring.length;
+    if(substringLength > this.length){
+        throw Error("The length of the substring is bigger than searched string!");
+    }
+
+    //var substractedSubstring = this.slice(this.length - substringLength, substringLength);
+    var substractedSubstring = this.slice(this.length - substringLength);
+
+    return substring === substractedSubstring;
+}
+
+String.prototype.endsWith = endsWith;
+
+function left(count){
+    if (count < this.length) {
+        return this.slice(0, count);
+    }
+
+    return this.toString();
+}
+
+String.prototype.left = left;
+
+function right(count){
+    if (count < this.length){
+        return this.slice(this.length - count);
+    }
+
+    return this.toString();
+}
+
+String.prototype.right = right;
+
+function padLeft(count, character){
+    var arr = Array.apply(null,Array(count)).map(function () {
+        return character || " "});
+
+    return arr.join("") + this;
+}
+
+String.prototype.padLeft = padLeft;
+
+function padRight(count, character){
+    var arr = Array.apply(null,Array(count)).map(function () {
+        return character || " "});
+
+    return this + arr.join("");
+}
+
+String.prototype.padRight = padRight;
+
+function repeat(count){
+    var string = this;
+    var arr = Array.apply(this,Array(count)).map(function () {return string});
+
+    return arr.join("");
+}
+
+String.prototype.repeat = repeat;
+
+var example = "This is an example string used only for demonstration purposes.";
+
+// Test: 'startsWith' extension method
+console.log("Extension method: 'startsWith'");
+console.log(example.startsWith("This"));
+console.log(example.startsWith("this"));
+console.log(example.startsWith("other"));
+console.log();
+
+// Test: 'endsWith'  extension method
+console.log("Extension method: 'endsWith'");
+console.log(example.endsWith("poses."));
+console.log(example.endsWith ("example"));
+console.log(example.startsWith("something else"));
+console.log();
+
+// Test: 'left' extension method
+console.log("Extension method: 'left'");
+console.log(example.left(9));
+console.log(example.left(90));
+console.log();
+
+// Test: 'right' extension method
+console.log("Extension method: 'right'");
+console.log(example.right(9));
+console.log(example.right(90));
+console.log();
+
+// Combinations must also work
+var example = "abcdefgh";
+console.log("Combinations test:");
+console.log(example.left(5).right(2));
+console.log();
+
+// Test: 'padLeft' extension method
+var hello = "hello";
+console.log("Extension method: 'padLeft'");
+console.log(hello.padLeft(5));
+console.log(hello.padLeft(10));
+console.log(hello.padLeft(5, "."));
+console.log(hello.padLeft(10, "."));
+console.log(hello.padLeft(2, "."));
+console.log();
+
+// Test: 'padRight' extension method
+console.log("Extension method: 'padRight'");
+console.log(hello.padRight(5));
+console.log(hello.padRight(10));
+console.log(hello.padRight(5, "."));
+console.log(hello.padRight(10, "."));
+console.log(hello.padRight(2, "."));
+console.log();
+
+// Test: 'repeat' extension method
+var character = "*";
+console.log("Extension method: 'repeat'");
+console.log(character.repeat(5));
+// Alternative syntax
+console.log("~".repeat(3));
+console.log();
+
+
+// Another combination
+console.log("Combinations test:");
+console.log("*".repeat(5));
+console.log("*".repeat(5).padLeft(10, "-").padRight(15, "+"));
---
+++ Advanced-JavaScript/04.Simulating-OOP/Problem-5-Vector/script.js
@@ -0,0 +1,114 @@
+var Vector = (function(){
+    function Vector(arr){
+        if (!arr || arr.length === 0){
+            throw Error("Vector should have valid coordinates!");
+        }
+
+        this._coordinates = arr;
+        this.dimension = arr.length;
+    }
+
+    Vector.prototype.toString = function(){
+        return "(" + this._coordinates.join(", ") + ")";
+    };
+
+    Vector.prototype.add = function(anotherVector){
+        validateDimensions(this.dimension, anotherVector.dimension);
+
+        var newVectorCoordinates = this._coordinates.map(function (number, index) {
+            return number + anotherVector._coordinates[index];
+        });
+
+        return new Vector(newVectorCoordinates);
+    };
+
+    Vector.prototype.substract = function(anotherVector){
+        validateDimensions(this.dimension, anotherVector.dimension);
+
+        var newVectorCoordinates = this._coordinates.map(function (number, index) {
+            return number - anotherVector._coordinates[index];
+        });
+
+        return new Vector(newVectorCoordinates);
+    };
+
+    Vector.prototype.dot = function(anotherVector){
+        validateDimensions(this.dimension, anotherVector.dimension);
+
+        return multiplyTwoArrays(this._coordinates, anotherVector._coordinates);
+    };
+
+    Vector.prototype.norm = function(){
+        return Math.sqrt(multiplyElementsInArray(this._coordinates));
+    };
+
+    // private functions
+    function validateDimensions(dimension1, dimension2){
+        if (dimension1 !== dimension2){
+            throw Error("Vectors must have same dimensions!");
+        }
+    }
+
+    function multiplyTwoArrays(firstArr, secondArr){
+        var sum = 0;
+        var length = firstArr.length;
+        for (var i = 0; i < length; i++) {
+            sum += firstArr[i] * secondArr[i];
+        }
+
+        return sum;
+    }
+
+    function multiplyElementsInArray(arr){
+        var sum = 0;
+        var length = arr.length;
+        for (var i = 0; i < length; i++) {
+            sum += arr[i] * arr[i];
+        }
+
+        return sum;
+    }
+
+    return Vector;
+})();
+
+// Test: print Vectors
+console.log("Test 'toString' method: ");
+var a = new Vector([1, 2, 3]);
+var b = new Vector([4, 5, 6]);
+var c = new Vector([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]);
+console.log(a.toString());
+console.log(c.toString());
+console.log();
+
+// The following throw errors
+//var wrong = new Vector();
+//var anotherWrong = new Vector([]);
+
+
+// Test: add two Vectors
+console.log("Test 'add' method: ");
+var result = a.add(b);
+console.log(result.toString());
+//a.add(c); // Error
+console.log();
+
+// Test: substract two Vectors
+console.log("Test 'substract' method: ");
+result = a.substract(b);
+console.log(result.toString());
+//a.subtract(c); // Error
+console.log();
+
+// Test: multiply two Vectors
+console.log("Test 'dot' method: ");
+result = a.dot(b);
+console.log(result.toString());
+console.log();
+
+// Test: norm Vector
+console.log("Test 'norm' method: ");
+console.log(a.norm());
+console.log(b.norm());
+console.log(c.norm());
+console.log(a.add(b).norm());
